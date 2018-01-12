83b833e7457401afc81d31189847ded9d1666375
hue9010 <hue9010@gmail.com>
2018-01-13 02:29:36 +0900

    ApiUserControllerTest로 이동한 테스트 코드 삭제

--- src/test/java/codesquad/web/UserAcceptanceTest.java
+++ src/test/java/codesquad/web/UserAcceptanceTest.java
@@ -3,8 +3,6 @@ package codesquad.web;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-
 import javax.annotation.Resource;
 
 import org.junit.Test;
@@ -15,14 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import codesquad.domain.UserRepository;
 
@@ -49,35 +42,6 @@ public class UserAcceptanceTest {
 		ResponseEntity<String> response = template.getForEntity("/create", String.class);
 		assertThat(response.getStatusCode(), is(HttpStatus.OK));
 		log.debug("body : {}", response.getBody());
-	}
-
-	@Test
-	public void create() throws Exception {
-		MultiValueMap<String, Object> params = createParams("testUser@korea.kr", "testUser", "password");
-		ResponseEntity<String> response = createPostResponse(params, "/create");
-
-		assertThat(response.getStatusCode(), is(HttpStatus.FOUND));
-		assertTrue(userRepository.findByEmail("testUser@korea.kr").isPresent());
-		assertThat(response.getHeaders().getLocation().getPath(), is("/login"));
-	}
-
-	private ResponseEntity<String> createPostResponse(MultiValueMap<String, Object> params, String path) {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params,
-				headers);
-
-		return template.postForEntity(path, request, String.class);
-	}
-
-	private MultiValueMap<String, Object> createParams(String email, String name, String password) {
-		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-		params.add("email", email);
-		params.add("name", name);
-		params.add("password", password);
-		return params;
 	}
 
 }
